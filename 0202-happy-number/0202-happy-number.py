7a1f76e5d82053e0e614406e0869b40848d108d9
geetanjalik01 <168562984+geetanjalik01@users.noreply.github.com>
2026-07-18 11:30:35 +0530

    Time: 0 ms (100%), Space: 19.3 MB (23.96%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        slow = n
+        fast = self.get_next(n)
+
+        while fast != 1 and slow != fast:
+            slow = self.get_next(slow)
+            fast = self.get_next(self.get_next(fast))
+
+        return fast == 1
+
+    def get_next(self, n: int) -> int:
+        total = 0
+        while n:
+            n, digit = divmod(n, 10)
+            total += digit * digit
+        return total
