155fd22819f810e435f623468717f76a552e6eec
geetanjalik01 <168562984+geetanjalik01@users.noreply.github.com>
2026-08-06 15:21:07 +0530

    Time: 477 ms (11.13%), Space: 0B (100%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,13 @@
+WITH DailySales AS (
+    SELECT 
+        visited_on,
+        SUM(amount) AS daily_amount
+    FROM Customer
+    GROUP BY visited_on
+)
+SELECT 
+    visited_on,
+    SUM(daily_amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS amount,
+    ROUND(AVG(daily_amount) OVER(ORDER BY visited_on ROWS BETWEEN 6 PRECEDING AND CURRENT ROW), 2) AS average_amount
+FROM DailySales
+LIMIT 18446744073709551615 OFFSET 6;
